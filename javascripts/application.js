142933af5e560d2669d7732caec8fabdb54d8ce1
Lewis Nixon <lewcfn@gmail.com>
2012-01-21 15:35:14 +0000

    detect support for media queries and load respond.js if no support is found

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,11 @@
 coldharbour = {	
 	initFunctionName: function() {
 		jQuery();
+		yepnope({
+		test : Modernizr.mq('@media only screen and (width)'),
+		yep  : '',
+		nope : 'libs/respond.min.js',
+		});
     }
 
 jQuery(document).ready(function() {
